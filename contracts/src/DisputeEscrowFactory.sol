72676d7c4a49a54574ba12ba1b90064718974bea
GiantDole <aak2229@columbia.edu>
2025-11-22 02:19:12 -0300

    Adding role system

--- contracts/src/DisputeEscrowFactory.sol
+++ contracts/src/DisputeEscrowFactory.sol
@@ -1,14 +1,29 @@
-contract DisputeEscrowFactory {
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./DisputeEscrow.sol";
+
+contract DisputeEscrowFactory is AccessControl {
+      // Role definitions
+      bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+      bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
+      bytes32 public constant DISPUTE_AGENT_ROLE = keccak256("DISPUTE_AGENT_ROLE");
+
       address public usdc;
 
       mapping(address => address) public serviceToEscrow;  // service address -> escrow contract
       mapping(address => bool) public isValidEscrow;       // escrow address -> is valid
-      address[] public allEscrows;
 
       event ServiceRegistered(address indexed service, address escrowContract, bytes publicKey);
+      event RoleGrantedToEscrow(address indexed escrowContract, bytes32 indexed role, address indexed account);
 
-      constructor(address _usdc) {
+      constructor(address _usdc, address _admin) {
           usdc = _usdc;
+
+          // Grant admin role to deployer
+          _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+          _grantRole(ADMIN_ROLE, _admin);
       }
 
       function registerService(
@@ -26,9 +41,50 @@ contract DisputeEscrowFactory {
 
           serviceToEscrow[sender] = escrowContract;
           isValidEscrow[escrowContract] = true;
-          allEscrows.push(escrowContract);
 
-          emit ServiceRegistered(service, escrowContract, publicKey);
+          emit ServiceRegistered(sender, escrowContract, publicKey);
+      }
+
+      // Admin functions
+      function setOperator(address operator) external onlyRole(ADMIN_ROLE) {
+          grantRole(OPERATOR_ROLE, operator);
+      }
+
+      function setFacilitator(address facilitator) external onlyRole(ADMIN_ROLE) {
+          grantRole(FACILITATOR_ROLE, facilitator);
+      }
+
+      function setDisputeAgent(address disputeAgent) external onlyRole(ADMIN_ROLE) {
+          grantRole(DISPUTE_AGENT_ROLE, disputeAgent);
+      }
+
+      function revokeOperator(address operator) external onlyRole(ADMIN_ROLE) {
+          revokeRole(OPERATOR_ROLE, operator);
+      }
+
+      function revokeFacilitator(address facilitator) external onlyRole(ADMIN_ROLE) {
+          revokeRole(FACILITATOR_ROLE, facilitator);
+      }
+
+      function revokeDisputeAgent(address disputeAgent) external onlyRole(ADMIN_ROLE) {
+          revokeRole(DISPUTE_AGENT_ROLE, disputeAgent);
+      }
+
+      // View functions
+      function getServiceEscrow(address service) external view returns (address) {
+          return serviceToEscrow[service];
+      }
+
+      function isOperator(address account) external view returns (bool) {
+          return hasRole(OPERATOR_ROLE, account);
+      }
+
+      function isFacilitator(address account) external view returns (bool) {
+          return hasRole(FACILITATOR_ROLE, account);
+      }
+
+      function isDisputeAgent(address account) external view returns (bool) {
+          return hasRole(DISPUTE_AGENT_ROLE, account);
       }
 
 }
